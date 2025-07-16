aaf817e3484d6bd4554cf8e90b7c3eed1e671fb5
Naga Jahnavi <150342894+Mnagajahnavi@users.noreply.github.com>
2025-07-16 12:41:26 +0530

    Update assets.js

--- admin/fooddelivery/src/assets/assets.js
+++ admin/fooddelivery/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = "https://tomato-a-food-delivery-app.onrender.com"
+export const url = 'https://tomato-a-food-delivery-app.onrender.com'
