c2455730791594bbf5b975044a512e7fd2656b64
Naga Jahnavi <150342894+Mnagajahnavi@users.noreply.github.com>
2025-07-16 12:01:11 +0530

    Update ShowContext.jsx

--- frontend/src/context/ShowContext.jsx
+++ frontend/src/context/ShowContext.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 const StoreContextProvider = (props) => {
   const [cartItem, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = "https://tomato-a-food-delivery-app.onrender.com";
   const [token,setToken] = useState("");
   const [food_list,setFoodList] = useState([]);
   const addToCart = async (itemId) => {
